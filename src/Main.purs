9d78975ae531cf68654867fca278b5b0ac41cf18
Janne Liuhtonen <janne.liuhtonen@reaktor.fi>
2015-09-28 18:49:08 +0300

    Wow such json

--- src/Main.purs
+++ src/Main.purs
@@ -1,30 +1,101 @@
 module Main where
 
-import qualified Control.Monad.Eff as E
+import Control.Alt
+import Control.Monad.Eff
+import Control.Monad.Eff.Class
+import Control.Monad.ST
 import qualified Control.Monad.Eff.Console as EC
+import Control.Monad.Eff.Exception
 import qualified Node.Encoding as Encoding
-import qualified Node.Buffer as Buffer
+import Node.Buffer (Buffer(), toString)
 import Control.Monad.Aff
 import Control.Monad.Aff.Console
-import Data.Maybe
+import Data.Either
+import Data.List
+import qualified Data.StrMap as M
+import Data.Argonaut.Core hiding (toString)
+import Data.Argonaut.Parser
+import Data.Argonaut.Printer
+import Data.Argonaut.Decode
+import Data.Argonaut.Combinators
 import Prelude
+import Data.Maybe
+import qualified Node.Datagram as U
 
-import Datagram.UDP
-import qualified Datagram.UDP.Aff as U
-
-socketType = "udp4"
+socketType = U.UDP4
 encoding = Encoding.UTF8
+listenPort = Just 62111
+listenInterfaces = Nothing -- all interfaces
 
-printMessage :: forall e. Buffer.Buffer -> RemoteAddressInfo -> E.Eff (console :: EC.CONSOLE | e) Unit
-printMessage msg rinfo = do 
-    EC.log $ show rinfo
-    EC.log $ Buffer.toString encoding msg
+newtype Member = Member {
+    nick :: String,
+    addr :: U.RemoteAddressInfo
+}
+
+newtype ServerState = ServerState {
+    members :: List Member    
+}
+
+newtype ClientMsg = ClientMsg {
+    "type" :: String,
+    "command" :: Command
+}
+                
+data Command = Connect ConnectObject | Chat ChatObject
+
+newtype ConnectObject = ConnectObject {
+    nick :: String
+}
+
+newtype ChatObject = ChatObject {
+    chatMsg :: String
+}
+
+instance decodeConnectMsg :: DecodeJson ConnectObject where
+    decodeJson json = do
+        obj <- decodeJson json
+        n <- obj .? "nick"
+        pure $ ConnectObject { nick: n }
+
+instance decodeChatMsg :: DecodeJson ChatObject where
+    decodeJson json = do
+        obj <- decodeJson json
+        n <- obj .? "chatMsg"
+        pure $ ChatObject { chatMsg: n }
+
+instance decodeClientMsg :: DecodeJson Command where
+    decodeJson json = (Connect <$> decodeJson json) <|> (Chat <$> decodeJson json) <|> (Left "NONONO")
 
 main = launchAff $ do
-  socket <- U.createSocket socketType
-  addrInfo <- U.bind (Just 62111) Nothing socket
-  log $ show addrInfo
-  U.onMessage printMessage socket
-  let buffer = Buffer.fromString "FOO" encoding
-  U.send buffer 0 (Buffer.size buffer) 41234 "127.0.0.1" socket
-  log "aww yiss"
+    socket <- U.createSocket socketType
+    U.onError logError socket
+    addrInfo <- U.bindSocket listenPort listenInterfaces socket
+    log $ show addrInfo
+    liftEff $ runServer socket
+    log "aww yiss"
+
+runServer :: forall e h. U.Socket -> Eff (socket :: U.SOCKET, console :: EC.CONSOLE | e) Unit
+runServer socket = runST do
+    serverState <- newSTRef { members: Nil }
+    let msgListener = \buf rinfo -> do
+            let msg = toString encoding buf
+            let parsedJson = parseIncomingMsg msg
+            case parsedJson of
+                 (Left errMsg) -> EC.log $ "Error parsing JSON: " ++ errMsg
+                 (Right (Chat (ChatObject o))) -> EC.log o.chatMsg
+                 (Right (Connect (ConnectObject { nick: nick }))) -> EC.log nick
+    runAff logError logListenStart $ U.onMessage msgListener socket
+
+parseIncomingMsg :: String -> Either String Command
+parseIncomingMsg msg = jsonParser msg >>= (decodeJson :: Json -> Either String Command)
+
+logListenStart :: forall eff. Unit -> Eff (console :: EC.CONSOLE | eff) Unit 
+logListenStart _ = EC.log "Listening for connections..."
+
+logMessage :: forall e. Buffer -> U.RemoteAddressInfo -> Eff (console :: EC.CONSOLE | e) Unit
+logMessage msg rinfo = do 
+    EC.log $ show rinfo
+    EC.log $ toString encoding msg
+
+logError :: forall e. Error -> Eff (console :: EC.CONSOLE | e) Unit
+logError err = EC.log $ "ERROR: " ++ message err
